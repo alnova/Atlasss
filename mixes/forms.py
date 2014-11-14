68d7d5d912daf3d64b159ccbb35883aa8057a7c4
Alessandra <alessandra@anoelting.com>
2014-11-13 23:48:51 -0500

    Atlas MVP
    edgar allen poe the raven

--- mixes/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from atlas.models import Greeting
-
-class CreateGreetingForm(forms.ModelForm):
-    class Meta:
-        model = Greeting
-        exclude = ['author', 'date']
--- mixes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('atlas.views',
-    (r'^$', 'list_greetings'),
-    (r'^sign$', 'create_greeting'),
-    (r'^model/(?P<terms>[A-z]+)/$', 'PhysicalModelView'),
-    (r'^json/stories/$', 'Stories'),
-)
